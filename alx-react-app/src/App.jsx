15a61b50da32b528b7f96c04ea9a88f1288fe82d
sunnywizy <sundayobasi222@gmail.com>
2025-11-12 06:21:32 +0100

    I just modefy my reactjs app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
       <div>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
   )
 }
