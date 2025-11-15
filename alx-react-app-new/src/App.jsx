8b7ad1b51a9e45ae4c56ceebf377ea9692ccd9d0
sunnywizy <sundayobasi222@gmail.com>
2025-11-15 08:11:27 +0100

    I just modefy my reactjs app

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+      <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+        <UserProfile 
+          name="Alice"
+          age="25" 
+          bio="ALoves hiking and photography" 
+        />
+
+        
+      </div>
+  )
+}
+
+export default App
