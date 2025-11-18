0743a09dcca90f207bda09096eea111508ae2693
sunnywizy <sundayobasi222@gmail.com>
2025-11-18 13:53:44 +0100

    I just modefy my reactjs app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import  RecipeList from './components/RecipeList'
 import  AddRecipeForm from './components/AddRecipeForm'
 
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
+    <>
       <div>
         <RecipeList />
         <AddRecipeForm />
       </div>
       
+    </>
   )
 }
 
