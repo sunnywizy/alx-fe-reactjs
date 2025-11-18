edbf94b1e4de7ea9269e8c0cffa8ed6169089d26
sunnywizy <sundayobasi222@gmail.com>
2025-11-18 15:46:47 +0100

    I just modefy my reactjs app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,27 @@
-import { useRecipeStore } from './recipeStore';
+import useRecipeStore from "./recipeStore";
+import { Link } from "react-router-dom";
+import AddToFavoritesButton from "./AddToFavoritesButton";
+import RemoveFromFavoritesButton from "./RemoveFromFavoritesButton";
 
-  const RecipeList = () => {
+const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
 
     return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
+        <div>
+            {recipes.map(recipe => {
+                const {id, title, description} = recipe;
+                return (
+                    <div key={id}>
+                        <h3>{title}</h3>
+                        <p>{description}</p>
+                        <Link to={`/${id}`}>Details</Link>
+                        <AddToFavoritesButton id={id} />
+                        <RemoveFromFavoritesButton id={id} />
+                    </div>
+                )
+            })}
+        </div>
     );
-  };
+};
+
 export default RecipeList;
