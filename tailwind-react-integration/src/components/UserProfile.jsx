8f78ee6c9de0d9cc2928e9ec5fe5e10029da9642
sunnywizy <sundayobasi222@gmail.com>
2025-11-30 07:34:42 +0100

    I just modify my ReactJs file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 md:max-w-sm max-w-xs max-w-xs mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
-      <img src="https://via.placeholder.com/150" alt="User" className=" w-36 h-36 rounded-full sm:w-24 sm:h-24 mx-auto md:w-36 md:h-36"/>
-      <h1 className=" text-sm md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+    <div className="user-profile bg-gray-100 p-8 md:max-w-sm max-w-xs max-w-xs mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8  hover:shadow-xl">
+      <img src="https://via.placeholder.com/150" alt="User" className=" w-36 h-36 rounded-full sm:w-24 sm:h-24 mx-auto md:w-36 md:h-36  hover:scale-110 transition-transform duration-300 ease-in-out"/>
+      <h1 className=" text-sm md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
       <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
